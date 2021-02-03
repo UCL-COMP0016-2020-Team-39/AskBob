98afcef7a364217b3b87b8d2681cd0833483a773
Jeremy <jeremyloyingping@googlemail.com>
2021-02-03 14:04:05 +0000

    Swap plugin and name order in the plugin wrapper.

--- askbob/plugin/__init__.py
+++ askbob/plugin/__init__.py
@@ -4,7 +4,7 @@ from typing import Text
 def action(plugin, name):
     def wrapper(action):
         def name_method(self) -> Text:
-            return "action_{}_{}".format(plugin, name)
+            return "action_{}_{}".format(name, plugin)
 
         setattr(action, "name", name_method)
         return action
