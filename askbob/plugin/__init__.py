abc815be3e8f4979ab96c56cc5a014c93fd6977b
Jeremy <jeremyloyingping@googlemail.com>
2021-02-11 23:05:07 +0000

    Improve plugin action decorator documentation.

--- askbob/plugin/__init__.py
+++ askbob/plugin/__init__.py
@@ -2,9 +2,19 @@ from typing import Text
 
 
 def action(plugin: str, name: str):
+    """A decorator for Ask Bob plugin actions (implementing the rasa_sdk.Action class).
+
+    Args:
+        plugin (str): The plugin name
+        name (str): The action name
+    """
+
     def wrapper(action):
         def name_method(self) -> Text:
-            return "action_{}_{}".format(name, plugin)
+            if name.startswith("validate_"):
+                return "{}_{}".format(name, plugin)
+            else:
+                return "action_{}_{}".format(name, plugin)
 
         setattr(action, "name", name_method)
         return action
