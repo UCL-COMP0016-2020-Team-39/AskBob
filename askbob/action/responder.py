eedf4ebc9f5d44cb93718ec8d9bd85207862a87b
Jeremy <jeremyloyingping@googlemail.com>
2021-02-11 23:39:07 +0000

    Add Responder documentation.

--- askbob/action/responder.py
+++ askbob/action/responder.py
@@ -8,14 +8,38 @@ class ResponseService:
     """An interface for a response service, which produces responses for text queries."""
 
     def is_ready(self) -> bool:
+        """Returns whether the response service is ready to accept queries.
+
+        Returns:
+            bool: Whether or not the response server is ready to accept queries.
+        """
         return True
 
     async def handle(self, query: str, sender: str = "askbob"):
+        """Handles a queries and produces a response.
+
+        Args:
+            query (str): The user's query.
+            sender (str, optional): A unique sender identifier. Defaults to "askbob".
+
+        Raises:
+            NotImplementedError: This is just an interface - please use a concrete implementation!
+        """
+
         raise NotImplementedError(
             "The response service has not been implemented.")
 
 
 def yielder(m: Dict[Text, Any]) -> Dict[Text, Any]:
+    """A helper function to generate correct message responses shared between ResponseService implementations.
+
+    Args:
+        m (Dict[Text, Any]): The message returned by Rasa.
+
+    Returns:
+        Dict[Text, Any]: An AskBob-formatted response.
+    """
+
     if "text" in m:
         return {"text": m["text"]}
     elif "image" in m:
@@ -28,10 +52,19 @@ def yielder(m: Dict[Text, Any]) -> Dict[Text, Any]:
 
 
 class RasaResponseService(ResponseService):
+    """A ResponseService that uses Rasa running locally to respond to queries."""
 
     agent: Agent
 
     def __init__(self, model_dir: str = "data/rasa/models") -> None:
+        """Initialises the RasaResponseService.
+
+        The Rasa actions server required for custom actions to function is automatically started when this class is initialised.
+
+        Args:
+            model_dir (str, optional): The location of the Rasa model to load. Defaults to "data/rasa/models".
+        """
+
         # Action Server
         import sys
         import subprocess
@@ -45,6 +78,7 @@ class RasaResponseService(ResponseService):
         self.agent = Agent.load(model_path, action_endpoint=endpoint)
 
     def __del__(self):
+        """Terminates the Rasa action service when this object is destroyed as it is no longer needed."""
         self.action_server.terminate()
 
     def is_ready(self) -> bool:
@@ -56,6 +90,7 @@ class RasaResponseService(ResponseService):
 
 
 class HTTPResponseService(ResponseService):
+    """A ResponseService that uses Rasa's HTTP webhook to respond to queries."""
 
     endpoint: str
 
