1c1c54a1332298c30e42e5d2b8571450ed1bc991
Jeremy <jeremyloyingping@googlemail.com>
2021-02-12 23:04:28 +0000

    Improve error handling.
    I've added extra checks to ensure the runtime config.ini file  is imported properly.

--- askbob/__main__.py
+++ askbob/__main__.py
@@ -1,5 +1,8 @@
 
-if __name__ == '__main__':
+import logging
+
+
+def main():
     # Parse CLI arguments
     from askbob.util import make_argument_parser
     parser = make_argument_parser()
@@ -8,13 +11,22 @@ if __name__ == '__main__':
     # Parse runtime configuration file values
     import configparser
     config = configparser.ConfigParser()
-    config.read(args.config)
+
+    if not config.read(args.config):
+        logging.error(
+            f"The runtime configuration file {args.config} could not be loaded.")
+        return
 
     if args.setup:
         # Train and setup the assistant
         from .setup import setup
         setup(args, config)
     else:
+        if 'Rasa' not in config or 'model' not in config['Rasa']:
+            logging.error(
+                "Missing Rasa.model location in the runtime configuration file.")
+            return
+
         # Run the voice assistant
         from askbob.action.responder import RasaResponseService
         responder = RasaResponseService(config['Rasa']['model'])
@@ -28,3 +40,7 @@ if __name__ == '__main__':
             from askbob.loop import interactive_loop
             import asyncio
             asyncio.run(interactive_loop(args, config, responder))
+
+
+if __name__ == '__main__':
+    main()
