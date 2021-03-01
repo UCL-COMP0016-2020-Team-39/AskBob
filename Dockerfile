ce8bb5636971513031259b41689d07093733c6f5
Jeremy <jeremyloyingping@googlemail.com>
2021-03-01 01:02:29 +0000

    Update Dockerfile to use new installation procedure.
    I've updated the Dockerfile to install AskBob from the setup.py file using pip.

--- Dockerfile
+++ Dockerfile
@@ -3,22 +3,29 @@ FROM python:3.7-slim
 ARG DEBIAN_FRONTEND=noninteractive
 ARG ASKBOB_SETUP_CONFIG=.
 
+RUN apt update && apt install -y build-essential
+
 WORKDIR /askbob
+
+RUN mkdir data
 
 COPY ./askbob askbob
 COPY ./plugins plugins
-COPY ./requirements requirements
-COPY ./config.ini .
+COPY ./*.ini .
 COPY ./*.json .
+COPY ./README.md .
+COPY ./MANIFEST.in .
+COPY ./setup.py .
+COPY ./data/*.pbmm data
+COPY ./data/*.scorer data
 
 RUN python -m pip install -U pip setuptools wheel
 
-RUN python -m pip install --no-cache-dir -r requirements/common.txt
+RUN python -m pip install -e .[voice]
 RUN python -m spacy download en_core_web_md
-RUN python -m spacy link en_core_web_md en
 
 RUN python -m askbob --setup $ASKBOB_SETUP_CONFIG
 
-ENTRYPOINT python -m askbob -s
+ENTRYPOINT python -m askbob -s -v
 
 EXPOSE 8000
--- setup.py
+++ setup.py
@@ -5,6 +5,31 @@ from os import path
 with open(path.join(path.abspath(path.dirname(__file__)), 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+extras = {
+    "docs": [
+        "sphinx~=3.4.3",
+        "recommonmark~=0.7.1",
+        "sphinx-rtd-theme~=0.5.1"
+    ],
+    "voice": [
+        "deepspeech~=0.9.3",
+        "scipy>=1.1.0",
+        "webrtcvad~=2.0.10"
+    ],
+    "interactive": [
+        "halo~=0.0.18",
+        "pyaudio~=0.2.11",
+        "pyttsx3~=2.90"
+    ],
+    "test": [
+        "pytest~=6.2.1",
+        "pytest-cov~=2.11.1",
+        "pytest-asyncio~=0.14.0"
+    ]
+}
+
+extras["interactive"] += extras["voice"]
+
 setup(name='askbob',
       version='0.0.2',
       description='A customisable, federated, privacy-safe voice assistant deployable on low-power devices.',
@@ -21,25 +46,6 @@ setup(name='askbob',
           "sanic~=20.9.0",
           "coloredlogs~=10.0"
       ],
-      extras_require={
-          "docs": [
-              "sphinx~=3.4.3",
-              "recommonmark~=0.7.1",
-              "sphinx-rtd-theme~=0.5.1"
-          ],
-          "voice": [
-              "deepspeech~=0.9.3",
-              "halo~=0.0.18",
-              "scipy>=1.1.0",
-              "pyaudio~=0.2.11",
-              "pyttsx3~=2.90",
-              "webrtcvad~=2.0.10"
-          ],
-          "test": [
-              "pytest~=6.2.1",
-              "pytest-cov~=2.11.1",
-              "pytest-asyncio~=0.14.0"
-          ]
-      },
+      extras_require=extras,
       include_package_data=True,
       zip_safe=False)
