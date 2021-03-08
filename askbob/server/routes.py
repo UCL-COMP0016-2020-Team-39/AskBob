c9669f964801466d0e33a5950ffa8c4827dab681
Jeremy <jeremyloyingping@googlemail.com>
2021-03-08 18:54:43 +0000

    Allow requests to be set with the GET HTTP verb.
    I've allowed requests to be submitted as GET requests. I recommend users stick with POST requests. This is just a temporary workaround for the concierge team.

--- askbob/server/routes.py
+++ askbob/server/routes.py
@@ -6,11 +6,7 @@ def routes(app, responder, plugin_configs):
     async def hello(request):
         return text("Hi, there! I think you might be in the wrong place... Bob.")
 
-    @app.route("/query", methods=['POST', 'OPTIONS'])
-    async def query(request):
-        message = request.form.get('message')
-        sender = request.form.get('sender')
-
+    async def handle_query(message, sender):
         if not message or not sender:
             return json({
                 "error": "Both a 'message' and a 'sender' must be provided."
@@ -31,6 +27,20 @@ def routes(app, responder, plugin_configs):
             "messages": [
                 response async for response in responder.handle(message, sender)]
         })
+
+    @app.route("/query", methods=['GET'])
+    async def query_get(request):
+        message = request.args.get('message')
+        sender = request.args.get('sender')
+
+        return await handle_query(message, sender)
+
+    @app.route("/query", methods=['POST', 'OPTIONS'])
+    async def query(request):
+        message = request.form.get('message')
+        sender = request.form.get('sender')
+
+        return await handle_query(message, sender)
 
     @app.route("/skills", methods=['GET', 'OPTIONS'])
     async def skills(request):
